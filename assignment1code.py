8e78f391090a5a8760da117a5c7c822b4c01dc1e
bozheng.ma1 <bozheng.ma1@cs.ucalgary.ca>
2024-05-09 12:09:19 -0600

    Added code, screenshots, and readme

---
+++ assignment1code.py
@@ -0,0 +1,11 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Bo Zheng Ma
+
+studentName = "Bo Zheng Ma"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+print("My skills are in python, C++, and Javascript!")
